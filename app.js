67afa8188325b026210ff1bf24be40ccbcb83c42
Nicobelicko <juannicolasruizmunoz@gmail.com>
2025-02-05 19:56:17 -0500

    Creamos la funcion limpiar_lista_amigos para ocultar la lista cada que se de click en el botón de sortear amigos, también creamos la funcion sortearAmigo para seleccionar de forma aleatoria un amigo de la lista como amigo secreto, además de crear la función limpiar_amigo_secreto para ocultar el amigo seleccionado cada que se agregue más amigos a la lista.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = []
+let lista = document.getElementById('listaAmigos')
+let lista_amigo_secreto = document.getElementById('resultado')
 
 
 function agregarAmigo(){
@@ -21,10 +23,25 @@ function limpiar_input(){
 
 function mostrar_lista_amigos(){
     limpiar_lista_amigos()
-    
+    limpiar_amigo_secreto()
     for(i = 0; i< amigos.length; i++){
         var li = document.createElement('li');
         li.appendChild(document.createTextNode(amigos[i]));
         lista.appendChild(li)
     } 
+}
+
+function limpiar_lista_amigos(){
+    lista.innerHTML = ""
+}
+function limpiar_amigo_secreto(){
+    lista_amigo_secreto.innerHTML = ""
+}
+function sortearAmigo(){
+    var indice_aleatorio = Math.floor(Math.random()*amigos.length);
+    var amigo_secreto = amigos[indice_aleatorio]
+    var resultado = `El amigo secreto sorteado es: ${amigo_secreto}`
+    
+    limpiar_lista_amigos()
+    lista_amigo_secreto.innerHTML = resultado
 }
