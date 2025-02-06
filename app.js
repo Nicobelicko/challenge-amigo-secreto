cdd0857858ab32e1a73dda842428329094833e3a
Nicobelicko <juannicolasruizmunoz@gmail.com>
2025-02-05 19:49:45 -0500

    creamos la funcion para agregar amigos, validamos que no se ingresen nombres vacíos y creamos una función con la que limpiamos el input

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = []
+
+
+function agregarAmigo(){
+    nombre = document.getElementById('amigo').value;
+    if(nombre === ''){
+        alert('Por favor, inserte un nombre.')
+    }
+    else{
+        amigos.push(nombre)
+        limpiar_input()
+        
+    }
+}
+
+function limpiar_input(){
+    input = document.getElementById('amigo')
+    input.value = '';
+}
